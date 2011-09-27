a68bb8cd2b51694c4573c14be4c7adc2fcfc3f66
yui-plug <red.blaez@gmail.com>
2011-09-26 18:51:53 -0700

    Fix for Ticket #2527949

--- src/loader/js/loader.js
+++ src/loader/js/loader.js
@@ -520,6 +520,16 @@ Y.Loader = function(o) {
      */
     // self.loadOptional = false;
 
+   /**
+     * Specify whether or not to load optional dependencies for
+     * a specific modules. 
+     * ex. {"history-html5":false, "autocomplete-sources":true}
+     * Overrides loadOptional.
+     * @property loadOptionalCfg
+     * @type {string:boolean}
+     * 
+     */
+    self.loadOptionalCfg={} 	;
     /**
      * All of the derived dependencies in sorted order, which
      * will be populated when either calculate() or insert()
@@ -1220,7 +1230,7 @@ Y.Loader.prototype = {
             intl = mod.lang || mod.intl,
             info = this.moduleInfo,
             ftests = Y.Features && Y.Features.tests.load,
-            hash;
+            hash, isLoadOptional;
 
         // console.log(name);
 
@@ -1305,7 +1315,9 @@ Y.Loader.prototype = {
             }
         }
 
-        if (o && this.loadOptional) {
+	isLoadOptional = (typeof this.loadOptionalCfg[name] == "undefined")? this.loadOptional: this.loadOptionalCfg[name];
+
+        if (o && (isLoadOptional == true)) {
             for (i = 0; i < o.length; i++) {
                 if (!hash[o[i]]) {
                     d.push(o[i]);
